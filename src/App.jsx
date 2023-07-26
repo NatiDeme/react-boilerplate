10c20a0bc90edc4f965e71f782e53f1dd30b0e4e
NatiDeme <natidemelashnatnael@hotmail.com>
2023-07-26 13:59:53 +0200

    Adding normal test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 function App() {
-  return <div></div>;
+//   let data
+//   const me = async () => {
+//      data = await fetch('https://boundaries-api.io/api/belgium/attributes/find/level/?level=4').then((res) => res.json()).then((data) => me2(data))
+//   }
+//   let print = []
+//   async function me2(data){
+//     console.log(data)
+//     for(let i = 0; i< data.attributes.length; i++){
+//       print.push(data.attributes[i].id)
+//     }
+//     console.log(print.join('*'))
+//  }
+//  me()
+  return <div>
+    This is for quick start
+  </div>;
 }
 
 export default App;
